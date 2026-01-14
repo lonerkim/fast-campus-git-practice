cfc98a019ce16e5d3d6ec48a25649b70d46d5c70
lonerkim <asas1206@gmail.com>
2026-01-14 06:39:25 +0000

    feat: Added Condition - print() If i is Even Integer.

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-for _ in range(3):
-    print("Hello, world!") # for some reason fucntion need to call 3 times.
+for i in range(10):
+    if i % 2 == 0: # added condition, only print when even
+     print("Hello, world!") # for some reason fucntion need to call 3 times.
